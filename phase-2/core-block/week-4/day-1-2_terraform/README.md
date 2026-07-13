9e24097f9c95699544b87ea1cb2810a7579a123d
Nguyễn Quang Dũng <yungngq@gmail.com>
2026-07-13 09:53:36 +0700

    feat(w4d12): create a module named k8s-app

---
+++ phase-2/core-block/week-4/day-1-2_terraform/README.md
@@ -0,0 +1,35 @@
+# Task Submission Template
+
+> Mỗi task = 1 folder con + 1 PR/MR riêng. Copy template này vào `README.md` của task.
+
+## Task: `Week 4: IaC nâng cao + Monitoring + Security basics: Day 1&2 - Terraform`
+
+- **Intern**: `Nguyễn Quang Dũng`
+- **Phase / Week / Day**: `Phase 2 / Week 4 / Day 1 & 2`
+- **Branch**: `phase-2/week-4/day-1`
+- **Submitted at**: `2026-07-11 08:40` (timezone +07)
+- **Time spent**: `6h`
+
+## 1. Mục tiêu
+
+
+## 2. Cách chạy
+
+
+## 3. Kết quả
+
+## 4. Khó khăn & cách giải quyết
+
+Không có.
+
+## 5. Reference
+- [SpaceLift - Terraform Modules Tutorial: Why Use Them, Best Practices, and Scaling](https://www.youtube.com/watch?v=Te4ijEaUGyU)
+
+
+## 6. Self-check
+
+- [x] Code chạy được trên máy sạch.
+- [x] README có hướng dẫn run lại.
+- [x] Không hard-code secret.
+- [x] Commit message theo Conventional Commits.
+- [x] Đã review lại code 1 lượt.
---
+++ phase-2/core-block/week-4/day-1-2_terraform/modules/k8s-app/main.tf
@@ -0,0 +1,97 @@
+resource "kubernetes_deployment" "app" {
+  metadata {
+    name = "${var.app_name}-deployment"
+    labels = {
+      app = var.app_name
+      env = var.env
+    }
+  }
+
+  spec {
+    replicas = var.replicas
+
+    selector {
+      match_labels = {
+        app = var.app_name
+        env = var.env
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = var.app_name
+          env = var.env
+        }
+      }
+
+      spec {
+        container {
+          image = var.image
+          name  = var.app_name
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "app" {
+  metadata {
+    name = "${var.app_name}-service"
+    labels = {
+      app = var.app_name
+      env = var.env
+    }
+  }
+
+  spec {
+    selector = {
+      app = var.app_name
+      env = var.env
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "ClusterIP"
+  }
+}
+
+resource "kubernetes_ingress_v1" "app" {
+  metadata {
+    name = "${var.app_name}-ingress"
+    labels = {
+      app = var.app_name
+      env = var.env
+    }
+    annotations = {
+      "kubernetes.io/ingress.class" = "nginx"
+    }
+  }
+
+  spec {
+    rule {
+      host = var.ingress_host
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = kubernetes_service.app.metadata[0].name
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
